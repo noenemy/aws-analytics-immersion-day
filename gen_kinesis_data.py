415a65daed0dc214a57d9a3525c382464466c22e
Sungmin Kim <sungmk@amazon.com>
2020-02-21 06:41:01 +0000

    add dry-run option

--- gen_kinesis_data.py
+++ gen_kinesis_data.py
@@ -11,6 +11,7 @@ import base64
 import traceback
 import random
 import time
+import datetime
 
 import boto3
 
@@ -34,8 +35,21 @@ DELIMETER_BY_FORMAT = {
 
 
 def gen_records(options, reader):
+  def _adjust_date(dt):
+     n = len('yyyy-mm-dd_HH:')
+     today = datetime.datetime.today()
+     return '{}:{}'.format(today.strftime('%Y-%m-%d %H'), dt[n:])
+
   record_list = []
   for row in reader:
+    is_skip = random.randint(1, 47) % 19 < 5
+    if is_skip:
+      continue
+
+    if int(row['Quantity']) <= 0:
+      continue
+
+    row['InvoiceDate'] = _adjust_date(row['InvoiceDate'])
     if options.out_format in DELIMETER_BY_FORMAT:
        delimeter = DELIMETER_BY_FORMAT[options.out_format]
        data = delimeter.join([e for e in row.values()])
@@ -54,10 +68,14 @@ def gen_records(options, reader):
     yield record_list
 
 
-def put_records_to_firehose(options, records):
+def put_records_to_firehose(client, options, records):
   MAX_RETRY_COUNT = 3
 
   for data in records:
+    if options.dry_run:
+      print(data)
+      continue
+
     for _ in range(MAX_RETRY_COUNT):
       try:
         response = client.put_record(
@@ -74,13 +92,17 @@ def put_records_to_firehose(options, records):
       raise RuntimeError('[ERROR] Failed to put_records into stream: {}'.format(options.stream_name))
 
 
-def put_records_to_kinesis(options, records):
+def put_records_to_kinesis(client, options, records):
   MAX_RETRY_COUNT = 3
 
   payload_list = []
   for data in records:
     partition_key = 'part-{:05}'.format(random.randint(1, 1024))
     payload_list.append({'Data': data, 'PartitionKey': partition_key})
+
+  if options.dry_run:
+    print(json.dumps(payload_list, ensure_ascii=False))
+    return
 
   for _ in range(MAX_RETRY_COUNT):
     try:
@@ -101,6 +123,7 @@ def main():
   parser.add_argument('--service-name', default='kinesis', choices=['kinesis', 'firehose'])
   parser.add_argument('--stream-name', help='The name of the stream to put the data record into.')
   parser.add_argument('--max-count', default=10, type=int, help='The max number of records to put.')
+  parser.add_argument('--dry-run', action='store_true')
 
   options = parser.parse_args()
   with open(options.input_file, newline='') as csvfile:
@@ -108,9 +131,9 @@ def main():
     client = boto3.client(options.service_name)
     for records in gen_records(options, reader):
       if options.service_name == 'kinesis':
-        put_records_to_kinesis(options, records)
+        put_records_to_kinesis(client, options, records)
       elif options.service_name == 'firehose':
-        put_records_to_firehose(options, records)
+        put_records_to_firehose(client, options, records)
       else:
         pass
       time.sleep(random.choices([0.01, 0.03, 0.05, 0.07, 0.1])[-1])
