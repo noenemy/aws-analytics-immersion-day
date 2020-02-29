769d4ca57aa6191fb49760d1e3fc23f7bd9326b8
Sungmin Kim <sungmk@amazon.com>
2020-02-29 16:01:18 +0000

    merge small files by athena ctas query

--- src/main/python/MergeSmallFiles/athena_ctas.py
+++ src/main/python/MergeSmallFiles/athena_ctas.py
@@ -5,55 +5,111 @@
 import sys
 import os
 import datetime
+import time
+import random
 
 import boto3
 
-DATABASE = os.getenv('ATHENA_DATABASE')
+random.seed(47)
+
+DRY_RUN = (os.getenv('DRY_RUN', 'false').lower() == 'true')
+AWS_REGION = os.getenv('REGION_NAME', 'us-east-1')
+
+OLD_DATABASE = os.getenv('OLD_DATABASE')
 OLD_TABLE_NAME = os.getenv('OLD_TABLE_NAME')
+NEW_DATABASE = os.getenv('NEW_DATABASE')
 NEW_TABLE_NAME = os.getenv('NEW_TABLE_NAME')
 WORK_GROUP = os.getenv('WORK_GROUP', 'primary')
 OUTPUT_PREFIX = os.getenv('OUTPUT_PREFIX')
+STAGING_OUTPUT_PREFIX = os.getenv('STAGING_OUTPUT_PREFIX')
+COLUMN_NAMES = os.getenv('COLUMN_NAMES', '*')
 
-AWS_REGION = os.getenv('REGION_NAME', 'us-east-1')
+EXTERNAL_LOCATION_FMT = '''{output_prefix}/year={year}/month={month:02}/day={day:02}/hour={hour:02}/'''
 
-OUTPUT_LOC_FMT = '''{output_prefix}/year={year}/month={month:02}/day={day:02}/hour={hour:02}'''
-
-QUERY_FMT = '''CREATE TABLE {new_table_name}
+CTAS_QUERY_FMT = '''CREATE TABLE {new_database}.tmp_{new_table_name}
 WITH (
+  external_location='{location}',
   format = 'PARQUET',
   parquet_compression = 'SNAPPY')
-AS SELECT *
-FROM {old_table_name};
+AS SELECT {columns}
+FROM {old_database}.{old_table_name}
 WHERE year={year} AND month={month} AND day={day} AND hour={hour}
+WITH DATA
 '''
 
-
-def lambda_handler(event, context):
-  event_dt = datetime.datetime.strptime(event['time'], "%Y-%m-%dT%H:%M:%SZ")
-  basic_dt = event_dt - datetime.timedelta(hours=1)
+def run_drop_tmp_table(athena_client, basic_dt):
   year, month, day, hour = (basic_dt.year, basic_dt.month, basic_dt.day, basic_dt.hour)
 
-  output_location = OUTPUT_LOC_FMT.format(output_prefix=OUTPUT_PREFIX,
+  tmp_table_name = '{table}_{year}{month:02}{day:02}{hour:02}'.format(table=NEW_TABLE_NAME,
+      year=year, month=month, day=day, hour=hour)
+
+  output_location = '{}/tmp_{}'.format(STAGING_OUTPUT_PREFIX, tmp_table_name)
+  query = 'DROP TABLE IF EXISTS {database}.tmp_{table_name}'.format(database=NEW_DATABASE,
+      table_name=tmp_table_name)
+
+  print('[INFO] QueryString:\n{}'.format(query), file=sys.stderr)
+  print('[INFO] OutputLocation: {}'.format(output_location), file=sys.stderr)
+
+  if DRY_RUN:
+    print('[INFO] End of dry-run', file=sys.stderr)
+    return
+
+  response = athena_client.start_query_execution(
+    QueryString=query,
+    ResultConfiguration={
+      'OutputLocation': output_location
+    },
+    WorkGroup=WORK_GROUP
+  )
+  print('[INFO] QueryExecutionId: {}'.format(response['QueryExecutionId']), file=sys.stderr)
+
+
+def run_ctas(athena_client, basic_dt):
+  year, month, day, hour = (basic_dt.year, basic_dt.month, basic_dt.day, basic_dt.hour)
+
+  new_table_name = '{table}_{year}{month:02}{day:02}{hour:02}'.format(table=NEW_TABLE_NAME,
     year=year, month=month, day=day, hour=hour)
 
-  query = QUERY_FMT.format(new_table_name=NEW_TABLE_NAME, old_table_name=OLD_TABLE_NAME,
+  output_location = '{}/tmp_{}'.format(STAGING_OUTPUT_PREFIX, new_table_name)
+  external_location = EXTERNAL_LOCATION_FMT.format(output_prefix=OUTPUT_PREFIX,
     year=year, month=month, day=day, hour=hour)
 
-  print('[INFO] QueryString={}'.format(query), file=sys.stderr)
-  print('[INFO] ExternalLocation={}'.format(output_location), file=sys.stderr)
+  query = CTAS_QUERY_FMT.format(new_database=NEW_DATABASE, new_table_name=new_table_name,
+    old_database=OLD_DATABASE, old_table_name=OLD_TABLE_NAME, columns=COLUMN_NAMES,
+    year=year, month=month, day=day, hour=hour, location=external_location)
 
-  client = boto3.client('athena')
-  response = client.start_query_execution(
+  print('[INFO] QueryString:\n{}'.format(query), file=sys.stderr)
+  print('[INFO] ExternalLocation: {}'.format(external_location), file=sys.stderr)
+  print('[INFO] OutputLocation: {}'.format(output_location), file=sys.stderr)
+
+  if DRY_RUN:
+    print('[INFO] End of dry-run', file=sys.stderr)
+    return
+
+  response = athena_client.start_query_execution(
     QueryString=query,
     QueryExecutionContext={
-      'Database': DATABASE
+      'Database': NEW_DATABASE
     },
     ResultConfiguration={
       'OutputLocation': output_location
     },
     WorkGroup=WORK_GROUP
   )
-  print('[INFO] QueryExecutionId={}'.format(response['QueryExecutionId']), file=sys.stderr)
+  print('[INFO] QueryExecutionId: {}'.format(response['QueryExecutionId']), file=sys.stderr)
+
+
+def lambda_handler(event, context):
+  event_dt = datetime.datetime.strptime(event['time'], "%Y-%m-%dT%H:%M:%SZ")
+  prev_basic_dt, basic_dt = [event_dt - datetime.timedelta(hours=e) for e in (2, 1)]
+
+  client = boto3.client('athena')
+  run_drop_tmp_table(client, prev_basic_dt)
+
+  print('[INFO] Wait for a few seconds until dropping old table', file=sys.stderr)
+  time.sleep(10)
+
+  run_ctas(client, basic_dt)
 
 
 if __name__ == '__main__':
@@ -69,5 +125,5 @@ if __name__ == '__main__':
     ],
     "detail": {}
   }
-  print('[DEBUG] event:\n', event, file=sys.stderr)
+  print('[DEBUG] event:\n{}'.format(event), file=sys.stderr)
   lambda_handler(event, {})
