fe2b3edb83eeed18b294ef0bf410b80d5bcfc028
Sungmin Kim <sungmk@amazon.com>
2020-03-15 12:27:57 +0000

    add setup script for hands-on-lab

---
+++ set-up-hands-on-lab.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -
+
+WORK_DIR=$(cd $(dirname $0); pwd)
+TARGET_DIR=$(dirname ${WORK_DIR})
+
+OS_NAME=$(cat /etc/os-release | awk -F "=" '$1 == "NAME" { print $2 }')
+if [[ z"${OS_NAME}" == z"\"Amazon Linux AMI\"" ]];
+then
+  sudo yum -y update
+  sudo yum -y install python36
+  sudo pip-3.6 install -U boto3
+elif [[ z"${OS_NAME}" == z"\"Ubuntu\"" ]];
+then
+  sudo apt-get -y update
+  sudo apt-get -y install python3.6
+  sudo apt-get -y install python3-pip
+else
+  echo "[Unknown OS] You should install python3.6+, pip3+ for yourself!"
+  exit 0
+fi
+
+ln -sf ${WORK_DIR}/src/main/python/UpsertToES/upsert_to_es.py ${TARGET_DIR}/upsert_to_es.py
+ln -sf ${WORK_DIR}/src/main/python/MergeSmallFiles/athena_ctas.py ${TARGET_DIR}/athena_ctas.py
+ln -sf ${WORK_DIR}/src/main/python/utils/gen_kinesis_data.py ${TARGET_DIR}/gen_kinesis_data.py
+ln -sf ${WORK_DIR}/resources ${TARGET_DIR}/resources
+
