a0b9a0d5c82f1c15f19da8a42b91d314d5bcf89a
EC2 Default User <ec2-user@ip-172-31-55-139.us-west-2.compute.internal>
2020-03-19 02:58:42 +0000

    update INFO log

--- src/main/python/utils/gen_kinesis_data.py
+++ src/main/python/utils/gen_kinesis_data.py
@@ -140,7 +140,7 @@ def main():
         print('\n'.join([e for e in records]))
       counter += 1
       if counter % 100 == 0:
-        print('[INFO] {} records are processed'.format(counter), file=sys.stderr)
+        print('[INFO] {} steps are processed'.format(counter), file=sys.stderr)
       time.sleep(random.choices([0.01, 0.03, 0.05, 0.07, 0.1])[-1])
 
 
